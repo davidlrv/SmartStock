8e5042bfe226bd4618d5c600d469e04c28dc70d2
david_ewbp3kz <david_ewbp3kz@DRIVAS>
2025-04-01 21:55:00 -0600

    Agregando vistas: Bodega y ListaBodega

---
+++ Controllers/InventarioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartStock.Controllers
+{
+    public class InventarioController : Controller
+    {
+        public IActionResult ListaBodega()
+        {
+            return View("~/Views/Inventario/Bodega/ListaBodega.cshtml");
+
+        }
+    }
+}
