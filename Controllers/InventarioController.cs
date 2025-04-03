37a2871fb0844a8803e8153a6b253e337c7d722b
david_ewbp3kz <david_ewbp3kz@DRIVAS>
2025-04-02 22:14:43 -0600

    Se agrego modelo bodega, lista bodega en el controlador inventario

--- Controllers/InventarioController.cs
+++ Controllers/InventarioController.cs
@@ -1,13 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartStock.Models;
+using SmartStock.Service;
 
 namespace SmartStock.Controllers
 {
     public class InventarioController : Controller
     {
-        public IActionResult ListaBodega()
-        {
-            return View("~/Views/Inventario/Bodega/ListaBodega.cshtml");
+        private readonly APIService _apiService;
+        private readonly IConfiguration _configuration;
 
+        public InventarioController(APIService apiService, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _apiService = apiService;
+        }
+        public async Task<IActionResult> ListaBodega()
+        {
+            var parametro = "{\"Estado\": \"1\"}";
+            var bodega = await _apiService.Run("sp_Mostrar_Bodega", parametro);
+
+            if (bodega.Contains("Data is Null"))
+            {
+                return View("~/Views/Inventario/Bodega/ListaBodega.cshtml");
+            }
+            else
+            {
+                ResponseDataBodega? _Bodega = JsonConvert.DeserializeObject<ResponseDataBodega>(bodega.ToString());
+                return View("~/Views/Inventario/Bodega/ListaBodega.cshtml", _Bodega?.DATA);
+            }
+        }
+        public async Task<IActionResult> Bodega(Bodega model)
+        {
+            return View("~/Views/Inventario/Bodega/Bodega.cshtml");
         }
     }
 }
---
+++ Models/Bodega.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+
+namespace SmartStock.Models
+{
+    public class Bodega
+    {
+
+        public int? ID_Bodega { get; set; }
+        public string Direccion { get; set; }
+        public string Cod_Bodega { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public bool Estado { get; set; } = true;
+
+        public string? Usuario { get; set; }
+    }
+
+    public class ResponseDataBodega
+    {
+        public List<Bodega> DATA { get; set; }
+    }
+
+    
+
+}
