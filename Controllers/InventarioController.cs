247f46557f10ecdbca72ba8429e8d3662a253857
david_ewbp3kz <david_ewbp3kz@DRIVAS>
2025-04-08 21:49:25 -0600

    Se realizo pantalla guardar bodega, se programo el boton guardar bodega y regresar listado bodega

--- Controllers/InventarioController.cs
+++ Controllers/InventarioController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SmartStock.Models;
 using SmartStock.Service;
@@ -30,9 +31,61 @@ namespace SmartStock.Controllers
                 return View("~/Views/Inventario/Bodega/ListaBodega.cshtml", _Bodega?.DATA);
             }
         }
+       
         public async Task<IActionResult> Bodega(Bodega model)
         {
-            return View("~/Views/Inventario/Bodega/Bodega.cshtml");
+            if (model.ID_Bodega == null)
+            {
+                model.Estado = true;
+                return View("~/Views/Inventario/Bodega/Bodega.cshtml", model); // Retorna la vista de bodega sin datos
+            }
+            var bodega = await _apiService.Run("sp_Mostrar_Bodega", model);
+
+            if (bodega.Contains("Data is Null"))
+            {
+                model.Estado = true;
+                return View("~/Views/Inventario/Bodega/Bodega.cshtml",model); // Retorna la vista de bodega sin datos
+            }
+            else
+            {
+                ResponseDataBodega? _bodega = JsonConvert.DeserializeObject<ResponseDataBodega>(bodega.ToString());
+                model = _bodega.DATA[0];
+                ModelState.ClearValidationState(nameof(model.Nombre));
+                ModelState.ClearValidationState(nameof(model.Estado));
+                TryValidateModel(model);
+                if (!ModelState.IsValid)
+                {
+                    // Manejar el estado no válido del modelo si es necesario
+                    return View("~/Views/Inventario/Bodega/Bodega.cshtml",model);
+                }
+                else
+                    return View("~/Views/Inventario/Bodega/Bodega.cshtml",model); // Retorna la vista Perfil con los datos del usuario
+            }
         }
+
+        [Authorize]
+        public async Task<IActionResult> GuardarBodega(Bodega model)
+        {
+
+            var bodega = await _apiService.Run("sp_Guardar_Bodega", model);
+
+            if (bodega.Contains("Data is Null") || bodega.Contains("ErrorMessage"))
+            {
+                // Si hay un error, podrías enviar un mensaje de error a la vista, si lo deseas.
+                TempData["Message"] = "Hubo un error al guardar la bodega. Por favor, intenta de nuevo.";
+                TempData["MessageType"] = "success"; // Puedes utilizar esto para determinar el tipo de mensaje en la vista.
+                return RedirectToAction("Bodega", new { ID_Bodega = model.ID_Bodega });
+            }
+            else
+            {
+                // Guardado exitoso, pasamos un mensaje de éxito a la vista.
+                TempData["Message"] = "La Bodega se guardo correctamente.";
+                TempData["MessageType"] = "success"; // Esto es opcional, pero puede ser útil para definir el estilo del mensaje.
+                ResponseDataBodega? _bodega = JsonConvert.DeserializeObject<ResponseDataBodega>(bodega.ToString());
+                return RedirectToAction("Bodega", new { ID_Bodega = _bodega?.DATA[0].ID_Bodega });
+            }
+
+        }
+
     }
 }
