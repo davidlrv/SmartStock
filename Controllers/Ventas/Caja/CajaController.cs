13c9737c787283c294f16971e838ffaad4e5933a
david_ewbp3kz <david_ewbp3kz@DRIVAS>
2025-04-09 22:14:20 -0600

    se desarrollo vista caja y listacaja, tambien se desarrolo el controlador ventas se modifico LayoutB

---
+++ Controllers/Ventas/Caja/CajaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartStock.Controllers.Ventas.Caja
+{
+    public class CajaController : Controller
+    {
+        public IActionResult Caja()
+        {
+            return View("~/Views/Ventas/Caja/Caja.cshtml");
+        }
+    }
+}
