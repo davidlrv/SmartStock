71bce58c443d040b2454ab3fdb16c5883b39cd8d
Rafael Rivas <rafaelrrs2310@gmail.com>
2025-04-28 22:22:15 -0600

    Desarrollo de Controlador caja, listacaja, y el modelo Caja, actualización de menu inicio.

--- Controllers/Ventas/Caja/CajaController.cs
+++ Controllers/Ventas/Caja/CajaController.cs
@@ -1,12 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartStock.Models;
+using SmartStock.Service;
 
-namespace SmartStock.Controllers.Ventas.Caja
+namespace SmartStock.Controllers.Ventas
 {
     public class CajaController : Controller
     {
-        public IActionResult Caja()
+        private readonly APIService _apiService;
+        private readonly IConfiguration _configuration;
+
+        public CajaController(APIService apiService, IConfiguration configuration)
         {
-            return View("~/Views/Ventas/Caja/Caja.cshtml");
+            _configuration = configuration;
+            _apiService = apiService;
         }
+        public async Task<IActionResult> ListaCaja()
+        {
+            var parametro = "{\"Estado\": \"1\"}";
+            var caja = await _apiService.Run("sp_Mostrar_Caja", parametro);
+
+            if (caja.Contains("Data is Null"))
+            {
+                return View("~/Views/Ventas/Caja/ListaCaja.cshtml");
+            }
+            else
+            {
+                ResponseDataCaja? _caja = JsonConvert.DeserializeObject<ResponseDataCaja>(caja.ToString());
+                return View("~/Views/Ventas/Caja/ListaCaja.cshtml", _caja?.DATA);
+            }
+        }
+        public async Task<IActionResult> Caja(Caja model)
+        {
+            if (model.ID_Caja == null)
+            {
+                model.Estado = true;
+                return View("~/Views/Ventas/Caja/Caja.cshtml", model); // Retorna la vista de caja sin datos
+            }
+            var caja = await _apiService.Run("sp_Mostrar_Caja", model);
+
+            if (caja.Contains("Data is Null"))
+            {
+                model.Estado = true;
+                return View("~/Views/Ventas/Caja/Caja.cshtml", model); // Retorna la vista de caja sin datos
+            }
+            else
+            {
+                ResponseDataCaja? _caja = JsonConvert.DeserializeObject<ResponseDataCaja>(caja.ToString());
+                model = _caja.DATA[0];
+                ModelState.ClearValidationState(nameof(model.Cod_Caja));
+                ModelState.ClearValidationState(nameof(model.Estado));
+                TryValidateModel(model);
+                if (!ModelState.IsValid)
+                {
+                    // Manejar el estado no válido del modelo si es necesario
+                    return View("~/Views/Ventas/Caja/Caja.cshtml", model);
+                }
+                else
+                    return View("~/Views/Ventas/Caja/Caja.cshtml", model); // Retorna la vista Perfil con los datos del usuario
+            }
+        }
+       
     }
 }
---
+++ Models/Caja.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartStock.Models
+{
+    public class Caja
+    {
+        public int ID_Caja { get; set; }
+        public int ID_Usuario { get; set; }
+        public string? Usuario_Apertura { get; set; }
+        public string? Usuario_Cierre { get; set; }
+        public string Cod_Caja { get; set; }
+        public DateTime Fecha_Apertura { get; set; }
+        public DateTime Fecha_Cierre { get; set; }
+        public double Monto_Apertura { get; set; }
+        public double Ingresos { get; set; }
+        public double Devoluciones { get; set; }
+        public bool Estado { get; set; } = true;
+
+        public string? Usuario { get; set; }
+    }
+    public class ResponseDataCaja
+    {
+        public List<Caja> DATA { get; set; }
+    }
+}
