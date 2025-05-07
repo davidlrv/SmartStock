f685ffc8525878737c5eea4fde5830b77efd8d7c
Rafael Rivas <rafaelrrs2310@gmail.com>
2025-05-06 22:21:57 -0600

    Modifico lista caja y lista bodega, modifico clse caja

--- Models/Caja.cs
+++ Models/Caja.cs
@@ -3,15 +3,10 @@
     public class Caja
     {
         public int ID_Caja { get; set; }
-        public int ID_Usuario { get; set; }
-        public string? Usuario_Apertura { get; set; }
-        public string? Usuario_Cierre { get; set; }
+        public int NombreCaja { get; set; }
+      
         public string Cod_Caja { get; set; }
-        public DateTime Fecha_Apertura { get; set; }
-        public DateTime Fecha_Cierre { get; set; }
-        public double Monto_Apertura { get; set; }
-        public double Ingresos { get; set; }
-        public double Devoluciones { get; set; }
+        
         public bool Estado { get; set; } = true;
 
         public string? Usuario { get; set; }
