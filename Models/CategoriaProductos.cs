f1842304308ec93db20546963f6321cc23f379a3
David Rivas <davidlrvg@gmail.com>
2026-03-13 23:45:17 -0600

    Categoria Producto Listo

---
+++ Models/CategoriaProductos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SmartStock.Models
+{
+    public class CategoriaProductos
+    {
+        public int? ID_Categoria { get; set; }
+
+        public string Categoria { get; set; }
+
+        public bool Estado { get; set; } = true;
+
+        public string? Usuario { get; set; }
+    }
+
+    public class ResponseDataCategoriaProductos
+    {
+        public List<CategoriaProductos> DATA { get; set; }
+    }
+}
