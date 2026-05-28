94ae8dea6295e5667b90e8ced1e03466a73b03bf
David Rivas <davidlrvg@gmail.com>
2026-05-27 22:57:15 -0600

    mejora

---
+++ Models/UnidadMedida.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartStock.Models
+{
+    public class UnidadMedida
+    {
+        public int? ID_UnidadMedida { get; set; }
+
+        public string Nombre_UnidadMedida { get; set; }
+
+        public bool Estado { get; set; } = true;
+
+        public string? Usuario { get; set; }
+    }
+
+    public class ResponseDataUnidadMedida
+    {
+        public List<UnidadMedida> DATA { get; set; }
+    }
+}
